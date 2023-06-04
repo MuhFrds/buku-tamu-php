e4e418a8214259f2fb11ce462b53f8497636e85b
Muh Firdaus <muhammadfirdaus66538@gmail.com>
2023-06-04 22:43:39 +0700

    update logini

--- cek_login.php
+++ cek_login.php
@@ -8,3 +8,28 @@ include "koneksi.php";
 
 @$pass = md5($_POST['password']);
 @$username = mysqli_escape_string($koneksi, $_POST['username']);
+@$password = mysqli_escape_string($koneksi, $pass);
+
+
+$login = mysqli_query($koneksi, "SELECT * FROM tuser where username = '$username' and password = '$password' and status = 'Aktif' ");
+
+
+$data = mysqli_fetch_array($login);
+
+
+// jika username dan password ditemukan/sesuai
+if ($data) {
+    $_SESSION['id_user'] = $data['id_user'];
+    $_SESSION['username'] = $data['username'];
+    $_SESSION['password'] = $data['password'];
+    $_SESSION['nama_pengguna'] = $data['nama_pengguna'];
+
+    // mengarahkan ke halaman admin
+    header('location:admin.php');
+} else {
+    echo "
+    <script>
+        alert('Maaf, Login Gagal pastikan yang anda masukkan benar');
+        document.location='index.php';
+    </script>";
+}
