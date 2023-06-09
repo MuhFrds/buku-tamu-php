9eb9fe590a901ead3e0203f917b66b051074cacd
Muh Firdaus <muhammadfirdaus66538@gmail.com>
2023-06-09 14:31:45 +0700

    update

--- header.php
+++ header.php
@@ -35,6 +35,8 @@ if (
 
     <title>Sistem Informasi Buku Tamu | SMK Mutuharjo</title>
 
+    <!-- Buat Favicon -->
+    <link rel="icon" href="assets/img/logo-sekolah.png" type="image/x-icon">
     <!-- Custom fonts for this template-->
     <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
@@ -43,6 +45,10 @@ if (
     <link href="assets/css/sb-admin-2.min.css" rel="stylesheet">
     <!-- Custom styles for this page -->
     <link href="assets/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+
+    <!-- Tambahkan referensi ke Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
 
 </head>
 
